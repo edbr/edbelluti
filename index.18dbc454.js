76ef6596115bfb1bc379291f39e86eff99be6a56
Eduardo Ribeiro <ribeiro.ebb@gmail.com>
2024-01-24 09:13:52 -0800

    Updates

--- index.18dbc454.js
+++ index.18dbc454.js
@@ -581,20 +581,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"1SICI":[function(require,module,exports) {
 // Import all of Bootstrap's JS
 var _bootstrap = require("bootstrap");
-function authenticatePassword() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    // Check credentials (replace this with your authentication logic)
-    if (username === "d" && password === "7") {
-        // Redirect to html upon successful login
-        console.log("Redirecting...");
-        window.location.href = "./paginaz.html"; // Updated path
-        return false; // Prevent default form submission
-    } else {
-        alert("Invalid credentials. Please try again.");
-        return false; // Prevent default form submission
-    }
-}
 
 },{"bootstrap":"h36JB"}],"h36JB":[function(require,module,exports) {
 /*!
