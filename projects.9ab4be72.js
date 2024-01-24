e7537666e80bcca8c61e8d147dafe0f8462416c0
Eduardo Ribeiro <ribeiro.ebb@gmail.com>
2024-01-24 11:43:13 -0800

    Updates

--- projects.9ab4be72.js
+++
@@ -1,2 +0,0 @@
-window.authentication={authenticatePassword:function(){var e=document.getElementById("username").value,n=document.getElementById("password").value;return"d"===e&&"7"===n?(console.log("Redirecting..."),window.location.href="../paginaz.html"):alert("Invalid credentials. Please try again."),!1}};
-//# sourceMappingURL=projects.9ab4be72.js.map
